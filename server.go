b685cc45e7534f15f80cb3b26a8c6d8a41875d43
Michael Devey <mdevey+github@gmail.com>
2016-11-23 17:46:16 +1100

    Specify outbound IP for requests from localhost
    Attempt to handle requests from local docker containers correctly
    by comparing outbound IP and private networks.
    IP's on the same network as the outbound IP should still be
    returned correctly.

--- server.go
+++ server.go
@@ -7,18 +7,28 @@
 package main
 
 import (
+    "flag"
     "fmt"
     "net"
     "net/http"
+    "log"
+    "strings"
 )
+
+var myip string
 
 func ipHandler(w http.ResponseWriter, r *http.Request) {
     ip, _, err := net.SplitHostPort(r.RemoteAddr)
     if err == nil {
-        fmt.Fprintf(w, "%s", ip)
+        if ip == "127.0.0.1" ||
+           ip == "::1" ||
+           isPrivateSubnet(net.ParseIP(ip)) { //local docker containers etc
+           fmt.Fprintf(w, "%s", myip)
+        } else {
+           fmt.Fprintf(w, "%s", ip)
+        }
     }
 }
-
 
 func dnsHandler(w http.ResponseWriter, r *http.Request) {
     ip, _, err := net.SplitHostPort(r.RemoteAddr)
@@ -41,7 +51,71 @@ func dnsHandler(w http.ResponseWriter, r *http.Request) {
     }
 }
 
+func getIPNet(network string) *net.IPNet {
+    _, subnet, _ := net.ParseCIDR(network)
+    return subnet;
+}
+
+var privateCIDRs = []*net.IPNet {
+    getIPNet("10.0.0.0/8"),
+    getIPNet("172.16.0.0/12"),
+    getIPNet("192.168.0.0/16"),
+}
+
+//If 'myip' is in a private range, we assume you are using this service to find ip's in that range also.
+//TODO allow _multiple_ private ranges (most companies just use one, typically 10.0.0.0 or 192.168.0.0)
+func AllowPrivateSubnetForMyip() *net.IPNet {
+    var ip= net.ParseIP(myip)
+    for i, ipr := range privateCIDRs {
+      if ipr.Contains(ip) {
+          //remove ipr
+          privateCIDRs = append(privateCIDRs[:i], privateCIDRs[i+1:]...)
+          return ipr
+      }
+    }
+    return nil
+}
+
+// isPrivateSubnet - check to see if this ip is in a private subnet
+func isPrivateSubnet(ip net.IP) bool {
+    for _, r := range privateCIDRs {
+      if r.Contains(ip) {
+          return true
+      }
+    }
+    return false
+}
+
+// Get preferred outbound ip of this machine
+func getOutboundIP() string {
+    conn, err := net.Dial("udp", "8.8.8.8:80")
+    if err != nil {
+        log.Fatal(err)
+    }
+    defer conn.Close()
+
+    localAddr := conn.LocalAddr().String()
+    idx := strings.LastIndex(localAddr, ":")
+
+    return localAddr[0:idx]
+}
+
 func main() {
+    flag.Parse()
+
+    if flag.NArg() < 1 {
+        myip = getOutboundIP()
+        log.Println("Warning: Blindly choosing my IP of " + myip)
+    } else {
+        myip = flag.Arg(0)
+    }
+    log.Println("For IP's in private subnets I will return my IP: " + myip)
+
+    network := AllowPrivateSubnetForMyip()
+    if network != nil {
+      log.Println(network.String() + " private IPs are the exception and will also be returned.")
+    }
+
     http.HandleFunc("/", ipHandler)
     http.HandleFunc("/dns", dnsHandler)
     http.ListenAndServe(":8080", nil)
